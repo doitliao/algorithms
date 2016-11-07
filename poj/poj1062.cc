b8fa25f51af2489dca89870c3be61142910ab534
doitliao.pc <imipc25@gmail.com>
2016-11-07 13:42:08 -0500

    fix 1062

--- poj/poj1062.cc
+++ poj/poj1062.cc
@@ -19,38 +19,87 @@ struct Edge{
     from(from), to(to), weight(w){}
 };
 
+struct Vertex{
+  int level;
+  int min,max;
+  LLD price;
+  Vertex():level(0), min(0), max(0), price(0){}
+  Vertex(int level, LLD price, int limits):
+    level(level), min(level - limits),max(level+limits), price(price){}
+};
+
 struct Graph{
   int N;
-  vector<vector<Edge> > G;
+  vector<Edge> G;
+  vector<Vertex> V; // vic.level
   vector<LLD> dist;
-  vector<LLD> price;
-  vector<int> levels;
-  vector<bool> visited;
   int level_limits;
-  Graph(int N):N(N), G(N), dist(N), price(N), levels(N), visited(N) {}
-
-  void AddEdge(int from, int to, LLD w){
-    G[from].push_back(Edge(from, to, w));
+  Graph(int N):N(N), dist(N){
+    fill(dist.begin(), dist.end(), 0);
   }
 
-  LLD dfs(size_t s = 0, int l = 0, int r = 0){
-    visited[s] = true;
-    LLD min_pay = price[s];
-    for(size_t i = 0; i < G[s].size(); ++i){
-      Edge &e = G[s][i];
-      if(visited[e.to])continue;
-      int min_l = min(l, levels[e.to]);
-      int max_l = max(r, levels[e.to]);
-      if(levels[e.to] < l && (l - levels[e.to]) > level_limits){
-        continue;
-      }
-      LLD pay = dfs(e.to, min_l, max_l);
-      min_pay = min(min_pay, pay + e.weight);
-      cout<<"min_pay:"<<min_pay<<" pay:"<<pay<<" weight:"<<e.weight<<endl;
+  void AddEdge(int from, int to, LLD w){
+    G.push_back(Edge(from, to, w));
+  }
+
+  LLD dfs(int s, LLD cur, LLD length){
+    LLD ans = V[s].price;
+    int l_min, l_max;
+    dist[s] = 1;
+    for(size_t j = 0; j < G.size(); j++){
+      Edge &e = G[j];
+      if(s != e.from || dist[e.to] == 1)continue;
+      if(V[e.from].max < V[e.to].level ||
+          V[e.from].min > V[e.to].level)continue;
+      if(e.weight + length > cur)continue;
+      l_min = V[e.to].min;
+      l_max = V[e.to].max;
+      V[e.to].min = max(V[e.from].min, V[e.to].min);
+      V[e.to].max = min(V[e.from].max, V[e.to].max);
+      ans = min(dfs(e.to,
+            min(V[s].price + length,cur),
+            e.weight + length) + e.weight, ans);
+      V[e.to].min = l_min;
+      V[e.to].max = l_max;
     }
-    visited[s] = false;
-    cout<<"DFS:"<<min_pay<<endl;
-    return min_pay;
+    dist[s] = 0;
+    return ans;
+  }
+
+  LLD bellman_ford(int s = 0){
+    fill(dist.begin(), dist.end(), INF);
+
+    dist[s] = 0;
+    for(int i = 0; i < N; i++){
+      bool flag = false;
+      for(size_t j = 0; j < G.size(); j++){
+        Edge &e = G[j];
+        if(dist[e.from] == INF)continue;
+        if(V[e.from].max < V[e.to].level ||
+            V[e.from].min > V[e.to].level)continue;
+        LLD val = dist[e.from] + e.weight;
+        if(dist[e.to] > val){
+          dist[e.to] = val;
+          cout<<"WTF:"<<e.from<<" "<<e.to<<" "<<val<<endl;
+
+          V[e.to].min = max(V[e.from].min, V[e.to].min);
+          V[e.to].max = min(V[e.from].max, V[e.to].max);
+          flag = true;
+        }
+      }
+
+      if(!flag){
+        LLD ans = dist[0] + V[0].price;
+        cout<<"GRE:"<<ans<<endl;
+        for(int k = 1 ; k < N; k++){
+          if(dist[k] == INF)continue;
+          cout<<"GRE:"<<ans<<" "<<dist[k]<<" "<<V[k].price<<endl;
+          ans = min(ans, dist[k] + V[k].price);
+        }
+        return ans;
+      }
+    }
+    return 0;
   }
 };
 
@@ -61,8 +110,7 @@ int main(){
   net.level_limits = M;
   for(int i = 0; i < N; ++i){
     cin>>p>>l>>x;
-    net.price[i] = p;
-    net.levels[i] = l;
+    net.V.push_back(Vertex(l,p,M));
     while(x--){
       cin>>to>>sale;
       to--;
@@ -70,6 +118,7 @@ int main(){
     }
   }
 
-  cout<<net.dfs(0, net.levels[0], net.levels[0])<<endl;
+  //cout<<net.bellman_ford(0)<<endl;
+  cout<<net.dfs(0, net.V[0].price, 0)<<endl;
   return 0;
 }
